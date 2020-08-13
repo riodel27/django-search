ac5810e9e9e598ae0438346afff8e53ed70225aa
rpamaong <rpamaong@startechup.com>
2020-08-13 15:27:20 +0800

    initial commit

---
+++ cities/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from .models import City
+
+
+# Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class SearchResultsView(ListView):
+    model = City
+    template_name = 'search_results.html'
+    # queryset = City.objects.filter(name__icontains='Boston')
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list =  City.objects.filter(
+            Q(name__icontains=query) | Q(state__icontains=query)
+        )
+
+        return object_list
